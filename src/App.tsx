030e9b31493e784560d6ce2d46d18335ce6de1cb
Cedric Karungu <ckarungu921@gmail.com>
2025-02-06 07:16:49 +0200

    chore: App pages &b routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Login from './pages/auth/Login';
+import Profile from './pages/Profile';
+import Upload from './pages/Upload';
+import Settings from './pages/Settings';
+import SignUp from './pages/auth/SignUp';
 
 function App() {
   return (
     <div className='min-h-screen w-screen'>
-      <Navbar/>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/sign-up" element={<SignUp />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/upload" element={<Upload />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/sections/Home/Hero.tsx
@@ -0,0 +1,10 @@
+
+const Hero = () => {
+  return (
+    <div className='h-screen w-full'>
+      Hero
+    </div>
+  )
+}
+
+export default Hero;
